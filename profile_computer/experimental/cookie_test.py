8870ea43d03e4eb1189145ca5838754672899b50
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 11:37:12 -0700

    Add fib function

--- profile_computer/experimental/cookie_test.py
+++ profile_computer/experimental/cookie_test.py
@@ -1,6 +1,3 @@
-import time
-
-
 def some_function(n):
     """Performs a computationally intensive task with a string."""
     result = '.'
@@ -12,8 +9,5 @@ def some_function(n):
 def main():
     """Main function to demonstrate profiling with string."""
     n = 14
-    start_time = time.time()
     result = some_function(n)
-    end_time = time.time()
     print('Result:', result)
-    print('Elapsed time:', end_time - start_time, 'seconds')
--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -12,8 +12,4 @@ def my_function(n):
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
-    start_time = time.time()
     result = my_function(n)
-    end_time = time.time()
-    print('Result:', result)
-    print('Elapsed time:', end_time - start_time, 'seconds')
