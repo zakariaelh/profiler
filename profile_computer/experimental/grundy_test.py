21712fff528bc1023d436bd6a3e81bdc3f294487
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 13:57:22 -0700

    adding foo

--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -8,9 +8,20 @@ def my_function(n):
         result += i * i
     return result
 
+def foo(n):
+    result = 0
+    for i in range(n):
+        for j in range(i):
+            result += (i * j) % (j + 1)
+    return result
+
+
 
 
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
     result = my_function(n)
+
+    rs = foo(5000)
+    print(rs)
