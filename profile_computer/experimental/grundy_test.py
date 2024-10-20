52f8e707b40b80b48fc7477427db9293df966d55
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 12:15:01 -0700

    new fibonacci

--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -9,11 +9,26 @@ def my_function(n):
     return result
 
 
+def fibonacci_sum(n):
+    def fib(x):
+        if x <= 1:
+            return x
+        return fib(x - 1) + fib(x - 2)
+
+    total_sum = 0
+    for i in range(n + 1):
+        total_sum += fib(i)
+
+    return total_sum
+
+
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
     start_time = time.time()
     result = my_function(n)
+    res = fibonacci_sum(20)
+    print(res)
     end_time = time.time()
     print('Result:', result)
     print('Elapsed time:', end_time - start_time, 'seconds')
