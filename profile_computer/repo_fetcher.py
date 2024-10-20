7baed5c765567889adc66a82217568fc807dbbc5
Omar Salim Moussa <moussa.omar.salim@gmail.com>
2024-10-19 20:15:26 -0700

    Move the profile_computer out of experimental and add some boilerplate for the server.

---
+++ profile_computer/repo_fetcher.py
@@ -0,0 +1,51 @@
+import requests
+import os
+import subprocess
+import shutil
+import venv
+
+token = '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'
+
+def get_pr_info(owner, repo, pr_number):
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def create_sandbox(sandbox_name):
+    if os.path.exists(sandbox_name):
+        shutil.rmtree(sandbox_name)
+
+    os.mkdir(sandbox_name)
+    print(f"Sandbox directory '{sandbox_name}' created successfully.")
+
+def clone_repo(owner, repo, sandbox_name):
+    subprocess.run(["git", "clone", f"https://github.com/{owner}/{repo}", sandbox_name])
+
+def fetch_pr(sandbox_name, pr_info, pr_number):
+    head_ref = pr_info["head"]["ref"]
+    subprocess.run(["git", "fetch", "origin", f'pull/{pr_number}/head:{head_ref}'], cwd=sandbox_name)
+
+def checkout_changes(sandbox_name, pr_info):
+    head_ref = pr_info["head"]["ref"]  # Get the head branch from the PR info
+    subprocess.run(["git", "checkout", head_ref], cwd=sandbox_name)
+
+def clone_pr_to_sandbox(owner, repo, pr_number, sandbox_name):
+    pr_info = get_pr_info(owner, repo, pr_number)
+    create_sandbox(sandbox_name)
+    clone_repo(owner, repo, sandbox_name)
+    fetch_pr(sandbox_name, pr_info, pr_number)
+    checkout_changes(sandbox_name, pr_info)
+
+def create_venv(sandbox_name):
+    venv.create(f"{sandbox_name}/my_venv")
+    subprocess.run(["source", "my_venv/bin/activate"], cwd=sandbox_name)
+
+owner = 'zakariaelh'
+repo = 'profiler'
+sandbox_name = 'sandbox'
+pr_number = 4353
+
+
+clone_pr_to_sandbox(owner, repo, pr_number, sandbox_name)
