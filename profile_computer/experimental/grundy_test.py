a0b352cc96eeef14c21c46836751479a242b33c3
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 12:25:05 -0700

    new fib

--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -8,9 +8,20 @@ def my_function(n):
         result += i * i
     return result
 
+def fibonacci_sum(n):
+    def fib(x):
+        if x <= 1:
+            return x
+        return fib(x - 1) + fib(x - 2)
 
+    total_sum = 0
+    for i in range(n + 1):
+        total_sum += fib(i)
+
+    return total_sum
 
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
+    fib_sum = fibonacci_sum(20)
     result = my_function(n)
