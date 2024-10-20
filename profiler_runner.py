0f32f1056cf52bb19f4e19d788eb27a4d1d6f544
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 08:59:46 -0700

    combine

---
+++ profiler_runner.py
@@ -0,0 +1,26 @@
+import subprocess
+import decorator_adder
+import re 
+
+def remove_ansi_escape_sequences(text):
+    ansi_escape = re.compile(r'\x1b\[[0-9;]*[a-zA-Z]')
+    return ansi_escape.sub('', text)
+
+
+def get_latency_profile(file_paths_to_profile, entry_point, sandbox_name):
+    for file_path_to_profile in str.split(file_paths_to_profile, ','):
+        decorator_adder.add_decorator(file_path_to_profile, 'profile')
+    decorator_adder.add_decorator(entry_point, 'profile')
+
+    python_path = f"venv/bin/python"
+
+    profile_path = f'latency_profile.lprof'
+    profile_path_txt = f'latency_profile.txt'
+    # TODO: Suppress STDOUT from first subprocess.
+    subprocess.run(['ls', '-la'], cwd=sandbox_name)
+    subprocess.run(['kernprof', '-l', '-o', profile_path, entry_point], cwd=sandbox_name)
+    subprocess.run([python_path, '-m', 'line_profiler', '-rtmz', profile_path], stdout=open(profile_path_txt, 'w'), cwd=sandbox_name)
+    with open(profile_path_txt, 'r') as file:
+        profile_txt_file_contents = file.read()
+
+    return remove_ansi_escape_sequences(profile_txt_file_contents)
--- repo_fetcher.py
+++ repo_fetcher.py
@@ -6,11 +6,15 @@ import venv
 import sys
 import stat
 import profiler_runner
-import secret
+from codechanger import get_pr_files, generate_name
+from dotenv import load_dotenv
+
+load_dotenv()
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 
 def get_pr_info(owner, repo, pr_number):
     url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}"
-    headers = {"Authorization": f"Bearer {secret.token}"}
+    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
@@ -32,43 +36,54 @@ def fetch_pr(sandbox_name, pr_info, pr_number):
     head_ref = pr_info["head"]["ref"]
     subprocess.run(["git", "fetch", "origin", f'pull/{pr_number}/head:{head_ref}'], cwd=sandbox_name)
 
-def checkout_changes(sandbox_name, pr_info):
-    head_ref = pr_info["head"]["ref"]  # Get the head branch from the PR info
-    subprocess.run(["git", "checkout", head_ref], cwd=sandbox_name)
+def checkout_changes(sandbox_name, branch_name):
+    subprocess.run(["git", "checkout", branch_name], cwd=sandbox_name)
 
 def create_venv(venv_path):
     venv.create(venv_path, with_pip=True)
 
-def clone_pr_to_sandbox(owner, repo, pr_number, sandbox_name):
-    pr_info = get_pr_info(owner, repo, pr_number)
+def clone_pr_to_sandbox(owner, repo, pr_number, branch_name=None):
+    sandbox_name = generate_name(base_name="sandbox")
+
     create_sandbox(sandbox_name)
     clone_repo(owner, repo, sandbox_name)
-    fetch_pr(sandbox_name, pr_info, pr_number)
-    checkout_changes(sandbox_name, pr_info)
+
+    if not branch_name: 
+        pr_info = get_pr_info(owner, repo, pr_number)
+        fetch_pr(sandbox_name, pr_info, pr_number)
+        branch_name = pr_info["head"]["ref"]  # Get the head branch from the PR info
+    
+    checkout_changes(sandbox_name, branch_name)
+        
     venv_name = 'venv'
     venv_path = f"{sandbox_name}/{venv_name}"
     create_venv(venv_path)
 
     # Shit set from the sandbox.
     pip_path = venv_path + "/bin/pip"  # Linux/macOS
-    python_path = venv_path + "/bin/python"
+    
     if sys.platform == "win32":
         pip_path = ".\/" + venv_path + "/Scripts/pip.exe"
         python_path = ".\/" + venv_path + "/Scripts/python.exe"
+    
     requirements_path = f"{sandbox_name}/requirements.txt"
 
+    pr_files = get_pr_files(owner=owner, repo=repo, pr_number=pr_number)
     # Shit needing to be built from our prod.
-    file_paths_to_profile = "sandbox/profile_computer/experimental/cookie_test.py,sandbox/profile_computer/experimental/grundy_test.py"
-    entry_point = "sandbox/profile_computer/experimental/main_test.py"
+    file_paths_to_profile = ",".join([f"{sandbox_name}/{file['filename']}" for file in pr_files])
+    entry_point = "profile_computer/experimental/main_test.py"
 
     process = subprocess.Popen([pip_path, "install", "-r", requirements_path])
     process.wait()
     prf = profiler_runner.get_latency_profile(file_paths_to_profile, entry_point, sandbox_name)
-    print(prf)
 
-owner = 'zakariaelh'
-repo = 'profiler'
-sandbox_name = 'sandbox'
-pr_number = 1
+    return prf 
 
-clone_pr_to_sandbox(owner, repo, pr_number, sandbox_name)
+
+if __name__ == "__main__":
+    owner = 'zakariaelh'
+    repo = 'profiler'
+    sandbox_name = 'sandbox'
+    pr_number = 1
+
+    clone_pr_to_sandbox(owner, repo, pr_number)
