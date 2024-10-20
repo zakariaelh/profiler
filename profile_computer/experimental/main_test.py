b87d82c13db927dfc66cdd20d7f0d33d6abd65a1
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 11:46:17 -0700

    add fib function

--- profile_computer/experimental/main_test.py
+++ profile_computer/experimental/main_test.py
@@ -1,11 +1,13 @@
 from line_profiler import profile
 import grundy_test
 import cookie_test
+import fibonnaci
 
 
 def main():
     cookie_test.main()
     grundy_test.main()
+    fibonnaci.main()
 
 
 if __name__ == '__main__':
