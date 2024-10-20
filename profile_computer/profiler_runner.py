d96b1facb383e3fac1e35fc54f2c19f47a2e1757
Omar Salim Moussa <moussa.omar.salim@gmail.com>
2024-10-19 23:42:23 -0700

    Update the profiler. Make it a library. Support creating venv in sandbox

--- profile_computer/profiler_runner.py
+++ profile_computer/profiler_runner.py
@@ -1,19 +1,17 @@
 import subprocess
-import argparse
 import decorator_adder
 
-parser = argparse.ArgumentParser(description='Profile Runner Arguments.')
+def get_latency_profile(file_paths_to_profile, entry_point, sandbox):
+    for file_path_to_profile in str.split(file_paths_to_profile, ','):
+        decorator_adder.add_decorator(file_path_to_profile, 'profile')
+    decorator_adder.add_decorator(entry_point, 'profile')
 
-parser.add_argument('--file_paths_to_profile', action="store", dest='file_paths_to_profile', default='')
-parser.add_argument('--entry_point', action="store", dest='entry_point', default='')
+    profile_path = f'{sandbox}/latency_profile.lprof'
+    profile_path_txt = f'{sandbox}/latency_profile.txt'
+    # TODO: Suppress STDOUT from first subprocess.
+    subprocess.run(f'kernprof -l -o {profile_path} {entry_point}')
+    subprocess.run(f'python -m line_profiler -rtmz {profile_path}', stdout=open(profile_path_txt, 'w'))
+    with open(profile_path_txt, 'r') as file:
+        profile_txt_file_contents = file.read()
 
-args = parser.parse_args()
-
-for file_path_to_profile in str.split(args.file_paths_to_profile, ','):
-    decorator_adder.add_decorator(file_path_to_profile, 'profile')
-
-profile_path = 'latency_profile.lprof'
-profile_path_txt = 'latency_profile.txt'
-# TODO: Suppress STDOUT from first subprocess.
-subprocess.run(f'kernprof -l -o {profile_path} {args.entry_point}')
-subprocess.run(f'python -m line_profiler -rtmz {profile_path}', stdout=open(profile_path_txt, 'w'))
+    return profile_txt_file_contents
