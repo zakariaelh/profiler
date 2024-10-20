0ee668c1b6a7706c8f5aea767e2062e4a0cd1cd0
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 14:03:23 -0700

    add bar

--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -9,8 +9,16 @@ def my_function(n):
     return result
 
 
+def bar(n):
+    result = 0
+    for i in range(n):
+        for j in range(n):
+            result += (i * j) % 2
+    return result
+
 
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
     result = my_function(n)
+    inefficient_result = bar(1000)  # Using a smaller n for bar to avoid extremely long execution times
