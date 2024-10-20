d9c9ab8a25eec1996957f6fc48b65bdbf94cd8cd
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 12:19:17 -0700

    remove time logging

--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -9,11 +9,8 @@ def my_function(n):
     return result
 
 
+
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
-    start_time = time.time()
     result = my_function(n)
-    end_time = time.time()
-    print('Result:', result)
-    print('Elapsed time:', end_time - start_time, 'seconds')
