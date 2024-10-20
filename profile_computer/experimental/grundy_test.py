326cd416f080fe49b6ae3750757d11a1429669ab
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 14:19:39 -0700

    adding zee

--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -9,8 +9,18 @@ def my_function(n):
     return result
 
 
+def zee(n):
+    result = []
+    for i in range(n):
+        temp = []
+        for j in range(i):
+            temp.append(j)
+        result.append(sum(temp))
+    return sum(result)
+
 
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
     result = my_function(n)
+    res = zee(n // 1000)  # Using a smaller n to avoid excessive runtime
