fa88fa4e8b497f32e2f7f99c138d646b99d8a5af
Zakaria el hjouji <elhjouji.zakaria@gmail.com>
2024-10-20 13:02:14 -0700

    add dot product

--- profile_computer/experimental/grundy_test.py
+++ profile_computer/experimental/grundy_test.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 
 def my_function(n):
@@ -8,9 +9,28 @@ def my_function(n):
         result += i * i
     return result
 
+def dot_product(a, b):
+    """Computes the dot product of two vectors."""
+    if len(a) != len(b):
+        raise ValueError("Vectors must have the same length")
+    
+    result = 0
+    for i in range(len(a)):
+        temp = a[i]
+        for _ in range(b[i]):
+            result += temp
+    
+    return result
 
 
 def main():
     """Main function to demonstrate profiling."""
     n = 10000000
     result = my_function(n)
+
+    n = 100000
+    a = [random.random() for i in range(n)]
+    b = [random.random() for i in range(n)]
+
+    res = dot_product(a, b)
+    
